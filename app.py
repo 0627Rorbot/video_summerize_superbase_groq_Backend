28e7046b2ac8e3d9ac5919b9cf69d0e03db08103
rorbot0627 <zac273017@gmail.com>
2024-10-19 12:36:55 +0000

    add: add route hello

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ app = create_app()
 
 # Enable CORS for all routes
 # CORS(app)
-CORS(app, resources={r"/upload": {"origins": "https://video-summerize-superbase-groq-ui.onrender.com", "methods": ["POST", "OPTIONS"], "allow_headers": ["Content-Type"]}})
+CORS(app, resources={r"/upload": {"origins": "https://video-summerize-superbase-groq-ui.onrender.com/", "methods": ["POST", "OPTIONS"], "allow_headers": ["Content-Type"]}})
 
 app.config['DEBUG'] = True
 
-@app.route('/')
+@app.route('/hello')
 def index():
     return "Hello, World!"
 
